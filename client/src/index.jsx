a33bbab2af513d790c394bf6dc8367d8461b2b2d
paul-11 <mrpaulyi@gmail.com>
2020-03-26 11:18:58 -0700

    Reimplement the footer component.
    Decided to add the footer back in. Added a Footer.jsx component and moved the scroll function to a new file called scrollFunction.js.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar.jsx'
+import Footer from './components/Footer.jsx'
 
 ReactDOM.render(<NavBar />, document.getElementById('nav-promo'))
+ReactDOM.render(<Footer />, document.getElementById('footer'))
